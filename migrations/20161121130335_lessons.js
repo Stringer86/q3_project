0cf4beb724640cab824a4a150974044fe7b935ea
Chase-Klingel <chaseklingel@Chases-MacBook-Pro.local>
2016-11-21 14:39:20 -0800

    lessons migration built

--- migrations/20161121130335_lessons.js
+++ migrations/20161121130335_lessons.js
@@ -1,8 +1,23 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('lessons', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table.string('category');
+    table.string('title').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
+    table.boolean('published').notNullable();
+    table.text('body').notNullable().defaultTo('');
+    table.integer('favorites');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('lessons');
 };
