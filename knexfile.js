35efacaa212eecf8017ff1497291c2db6a5a646b
Stringer86 <stringer.stephen@gmail.com>
2016-11-21 17:03:17 -0800

    server set up

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,15 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/devConnect_dev'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/devConnect_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
